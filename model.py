d60dbd2bb819eb275371694e6e371f5e09f02de6
Simon890 <revellosimon890@gmail.com>
2023-10-14 12:46:41 -0300

    Cube texture helloworld

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from main import GraphicsEngine
+import glm
 
 class Triangle:
     def __init__(self, app : GraphicsEngine) -> None:
@@ -31,6 +32,96 @@ class Triangle:
         vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
         vertex_data = np.array(vertex_data, dtype="f4")
         return vertex_data
+    
+    def get_vbo(self):
+        vertex_data = self.get_vertex_data()
+        #Send data to the GPU memory
+        vbo = self.ctx.buffer(vertex_data)
+        return vbo
+    
+    def get_shader_program(self, shader_name):
+        with open(f"shaders/{shader_name}.vert") as file:
+            vertext_shader = file.read()
+        with open(f"shaders/{shader_name}.frag") as file:
+            fragment_shader = file.read()
+        
+        program = self.ctx.program(vertex_shader=vertext_shader, fragment_shader=fragment_shader)#Compile our shader using CPU
+        return program
+    
+class Cube:
+    def __init__(self, app : GraphicsEngine) -> None:
+        self.app = app
+        self.ctx = app.ctx
+        self.vbo = self.get_vbo()
+        self.shader_program = self.get_shader_program("default")
+        self.vao = self.get_vao()
+        self.m_model = self.get_model_matrix()
+        self.on_init()
+        
+    def update(self):
+        #Generate rotation and update the matrix_model
+        m_model = glm.rotate(self.m_model, self.app.time, glm.vec3(0, 1, 0))
+        self.shader_program["m_model"].write(m_model)
+        
+    def get_model_matrix(self):
+        #Generate matrix_model as an identity matrix 4x4
+        m_model = glm.mat4() #Identity matrix 4x4
+        return m_model
+    
+    def on_init(self):
+        #Exports variables to shaders
+        self.shader_program["m_proj"].write(self.app.camera.m_proj)
+        self.shader_program["m_view"].write(self.app.camera.m_view)
+        self.shader_program["m_model"].write(self.m_model)
+    
+    def render(self):
+        self.update()
+        self.vao.render()
+        
+    def destroy(self):
+        #We need to release the resources since OpenGL doesn't have a garbage collector
+        self.vbo.release()
+        self.shader_program.release()
+        self.vao.release()
+    
+    def get_vao(self):
+        #3f = Buffer format, in_position = Attributes
+        #3f each vertext is assign 3 floating numbers and these correspont to in_position attribute
+        #vao = Vertext Array Object => We associate the vertext object (VBO) with Shader program
+        #vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, "3f", "in_position")]) Before adding texture
+        vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, "2f 3f", 'in_texcoord_0', 'in_position')])
+        return vao
+    
+    def get_vertex_data(self):
+        #coordinates     x     y    z
+        #opengl uses right handed coordinate system
+        vertices = [(-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1),
+                       (-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, 1, -1)]
+        indices = [(0, 2, 3), (0, 1, 2), 
+                   (1, 7, 2), (1, 6, 7),
+                   (6, 5, 4), (4, 7, 6),
+                   (3, 4, 5), (3, 5, 0),
+                   (3, 7, 4), (3, 2, 7),
+                   (0, 6, 1), (0, 5, 6)]
+        vertex_data = self.get_data(vertices, indices)
+        vertex_data = np.array(vertex_data, dtype="f4")
+        
+        #Set texture
+        tex_coord = [(0, 0), (1, 0), (1, 1), (0, 1)]
+        tex_coord_indices = [(0, 2, 3), (0, 1, 2),
+                             (0, 2, 3), (0, 1, 2),
+                             (0, 1, 2), (2, 3, 0),
+                             (2, 3, 0), (2, 0, 1),
+                             (0, 2, 3), (0, 1, 2),
+                             (3, 1, 2), (3, 0, 1)]
+        text_coord_data = self.get_data(tex_coord, tex_coord_indices)
+        vertex_data = np.hstack([text_coord_data, vertex_data]) #Combine the geometry and texture data
+        return vertex_data
+    
+    @staticmethod
+    def get_data(vertices, indices):
+        data = [vertices[ind] for triange in indices for ind in triange]
+        return np.array(data, dtype="f4")
     
     def get_vbo(self):
         vertex_data = self.get_vertex_data()
--- shaders/default.vert
+++ shaders/default.vert
@@ -1,7 +1,15 @@
 #version 330 core
 
-layout (location = 0) in vec3 in_position; //Coordinates of the vertext as a 3d vector. This is related to the vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, "3f", "in_position")]) in model.py
+layout (location = 0) in vec2 in_texcoord_0;
+layout (location = 1) in vec3 in_position; //Coordinates of the vertext as a 3d vector. This is related to the vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, "3f", "in_position")]) in model.py
+
+out vec2 uv_0; //Texture coordinates for the fragment shader (default.frag)
+
+uniform mat4 m_proj;
+uniform mat4 m_view;
+uniform mat4 m_model;
 
 void main() {
-    gl_Position = vec4(in_position, 1.0);
+    uv_0 = in_texcoord_0;
+    gl_Position = m_proj * m_view * m_model * vec4(in_position, 1.0);
 }
